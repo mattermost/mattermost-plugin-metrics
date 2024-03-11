7d6983b9b757711ad9283f8299498f64517c3896
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2024-03-11 09:42:38 +0100

    [MM-57135] [MM-57136] some minor improvements before publishing (#18)
    * some minor improvements before publishing
    
    * make load plugin to actually get config from the server
    
    * add tsdb path setting to the plugin setting page
    
    * adjust local retention
    
    * add download icon to the download button

--- server/configuration.go
+++ server/configuration.go
@@ -27,7 +27,7 @@ import (
 // If you add non-reference types to your configuration struct, be sure to rewrite Clone as a deep
 // copy appropriate for your types.
 type configuration struct {
-	DBPath                         *string
+	DBPath                         *string `json:"dbpath"`
 	AllowOverlappingCompaction     *bool
 	EnableMemorySnapshotOnShutdown *bool
 	BodySizeLimitBytes             *int64
@@ -49,6 +49,8 @@ type configuration struct {
 	FileStoreSyncPeriodMinutes *int
 	// FileStoreCleanupPeriodMinutes is the period to run cleanup job in the filestore
 	FileStoreCleanupPeriodMinutes *int
+	// CollectMetricsFrom is the period to collect metrics to create the dump
+	CollectMetricsFrom *string `json:"collectmetricsfrom"`
 }
 
 func (c *configuration) SetDefaults() {
@@ -90,6 +92,9 @@ func (c *configuration) SetDefaults() {
 	}
 	if c.FileStoreCleanupPeriodMinutes == nil {
 		c.FileStoreCleanupPeriodMinutes = model.NewInt(120)
+	}
+	if c.CollectMetricsFrom == nil {
+		c.CollectMetricsFrom = model.NewString("3_days")
 	}
 }
 
--- server/const.go
+++ server/const.go
@@ -1,11 +1,14 @@
 package main
 
+import "time"
+
 const (
-	PluginName     = "mattermost-plugin-metrics"
-	tsdbDirName    = "data"
-	metaFileName   = "meta.json"
-	metaVersion1   = 1
-	pluginDataDir  = "plugin-data"
-	zipFileName    = "tsdb_dump.tar.gz"
-	MaxRequestSize = 5 * 1024 * 1024 // 5MB
+	PluginName         = "mattermost-plugin-metrics"
+	tsdbDirName        = "data"
+	metaFileName       = "meta.json"
+	metaVersion1       = 1
+	pluginDataDir      = "plugin-data"
+	zipFileName        = "tsdb_dump.tar.gz"
+	MaxRequestSize     = 5 * 1024 * 1024 // 5MB
+	localRetentionDays = 3 * 24 * time.Hour
 )
